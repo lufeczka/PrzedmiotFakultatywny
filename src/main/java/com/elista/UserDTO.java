0f9a4694381cbe19121a659872fb4c854ca299f7
maciej.chyczewski <thrundi@gmail.com>
2016-03-23 11:12:28 +0100

    Przykładowy UserDTO

--- src/main/java/com/elista/UserDTO.java
+++ src/main/java/com/elista/UserDTO.java
@@ -4,25 +4,34 @@ import io.swagger.annotations.ApiModel;
 import java.io.Serializable;
 import java.util.Date;
 
+
 /**
  * Created by maciej.chyczewski on 23-03-2016.
  */
 @ApiModel
 public class UserDTO implements Serializable {
     private Long id;
+    private String email;
+    private String password;
     private String name;
     private String lastName;
-    private Date registered;
+    private Date register_date;
+    private String phone;
+    private Boolean active;
 
     public UserDTO(){
 
     }
 
-    public UserDTO(Long id, String name, String lastName, Date registered){
+    public UserDTO(Long id, String email, String password, String name, String lastName, Date register_date, String phone, Boolean active) {
         this.id = id;
+        this.email = email;
+        this.password = password;
         this.name = name;
         this.lastName = lastName;
-        this.registered = registered;
+        this.register_date = register_date;
+        this.phone = phone;
+        this.active = active;
     }
 
     public Long getId() {
@@ -31,6 +40,18 @@ public class UserDTO implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getName() {
@@ -49,11 +70,24 @@ public class UserDTO implements Serializable {
         this.lastName = lastName;
     }
 
-    public Date getRegistered() {
-        return registered;
+    public Date getRegister_date() {
+        return register_date;
     }
 
-    public void setRegistered(Date registered) {
-        this.registered = registered;
+    public String getPhone() {
+        return phone;
     }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
 }
