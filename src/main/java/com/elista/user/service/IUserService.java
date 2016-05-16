b357d534eccaf982696efa445955bc5654c825f8
lufeczka <wojnies1@gmail.com>
2016-05-16 17:06:11 +0200

    user

--- src/main/java/com/elista/user/service/IUserService.java
+++ src/main/java/com/elista/user/service/IUserService.java
@@ -1,8 +1,11 @@
 package com.elista.user.service;
 
 import com.elista.user.ob.UserOB;
+import org.joda.time.DateTime;
 import org.jvnet.hk2.annotations.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by Thrundi on 2016-05-03.
@@ -12,4 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 public interface IUserService {
     UserOB saveUser (UserOB aUserOB);
     Boolean deleteUser (Long aId);
+    UserOB updateUser (Long aId, String aEmail, String aName, String aSurname, String aPhoneNumber, String aAddress, DateTime aActiveUntil);
+    Boolean updateUserPassword (Long aId, String oldPassword, String newPassword);
 }
