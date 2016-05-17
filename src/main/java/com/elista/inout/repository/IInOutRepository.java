a6f9c1b6a9b07cfd90a8f8b0f82e48aa7a6f928b
Maciej Chyczewski <thrundi@gmail.com>
2016-05-17 16:07:37 +0200

    Servisy

--- src/main/java/com/elista/inout/repository/IInOutRepository.java
+++ src/main/java/com/elista/inout/repository/IInOutRepository.java
@@ -4,6 +4,8 @@ import com.elista.inout.ob.InOutOB;
 import com.elista.user.ob.UserOB;
 import org.joda.time.DateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,4 +24,11 @@ public interface IInOutRepository extends JpaRepository<InOutOB, Long> {
     List<InOutOB> findByDateFromLessThanAndDateFromGreaterThan(DateTime dateFrom, DateTime dateFrom2);
     List<InOutOB> findByUser(UserOB user);
     List<InOutOB> findByUserAndDateToIsNull(UserOB user);
+//    @Query("SELECT SUM(FUNC('TIME_TO_SEC', inout.timeFrom)) from InOutOB inout JOIN inout.users u where u.id = ?1")
+//    List<DateTime> findTimeByUserId(@Param("user_id") Long id);
+    /*
+
+    @Query("SELECT ps FROM PlanSpecOB ps JOIN ps.plan p JOIN p.users u where u.id = ?1")
+    List<PlanSpecOB> findByUserId(@Param("user_id") Long id);
+     */
 }
